7b87a920f69f6376d962c66d841dca6dcdada99f
Sean Quillen <sean.r.quillen@gmail.com>
2021-02-12 10:48:01 -0600

    add route components

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,27 @@
-import logo from '../logo.svg';
-import '../styles/App.css';
+import React from 'react';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
+import Login from './Login';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route
+            exact
+            path="/create"
+            component={CreateLink}
+          />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
